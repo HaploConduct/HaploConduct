6c4aaebea8fb210702f841e98e148261718cbe8b
jbaaijens <baaijens@cwi.nl>
2017-07-17 17:54:16 +0200

    removing sfo, using rust-overlaps instead

--- sfo_2011_5/libcds/src/static_bitsequence/static_bitsequence_builder_sdarray.cpp
+++
@@ -1,7 +0,0 @@
-
-#include <static_bitsequence_builder_sdarray.h>
-
-static_bitsequence * static_bitsequence_builder_sdarray::build(uint * buff, uint len) {
-	return new static_bitsequence_sdarray(buff,len);
-}
-
