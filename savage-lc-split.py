9c58003b5b1feae7afe9068fecbfc641fe15795d
jbaaijens <baaijens@cwi.nl>
2018-03-15 09:55:51 +0100

    remove subdirectories of regions with too little (<100) reads

--- savage-lc-split.py
+++ savage-lc-split.py
@@ -317,6 +317,8 @@ Author: %s
                 subprocess.check_call('samtools view -F4 -bh assembly/s_p1_p2.bam %s:%s-%s | samtools fastq - 1> %s/assembly/s_p1_p2.fastq 2>/dev/null' % (chrom, region_lb, region_ub, dirname), shell=True)
                 if file_len('%s/assembly/s_p1_p2.fastq' % dirname) >= 400:
                     final_split.append([region_lb, region_ub])
+                else:
+                    subprocess.check_call('rm -rf %s' % dirname, shell=True)
                 while idx < len(regions) and regions[idx][1] < pos:
                     idx += 1
                 pos += args.split_size
